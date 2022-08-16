8e3dd9c415d63085b472c9e6aa9bf6739cae2132
Rodrigo Alanis Lemus <rodrigoalanislemus@Rodrigos-MacBook-Air.local>
2022-08-16 16:35:36 -0500

    Navbar Responsive And Breakpoints Setup

--- components/molecules/Navbar.tsx
+++ components/molecules/Navbar.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
+  faMagnifyingGlass,
   faShoppingCart,
   faUser,
 } from "@fortawesome/free-solid-svg-icons";
@@ -18,10 +19,22 @@ export const Navbar = () => {
           <FontAwesomeIcon
             icon={faBars}
             className={styles["navbar__bars"]}
-            onClick={() => { 
-              setExtraMenu(!extraMenu) 
+            onClick={() => {
+              setExtraMenu(!extraMenu);
             }}
           />
+
+          <div className={styles["navbar__search"]}>
+            <FontAwesomeIcon
+              icon={faMagnifyingGlass}
+              className={styles["navbar__search-icon"]}
+            />
+            <input
+              type="text"
+              placeholder="Buscar"
+              className={styles["navbar__search-input"]}
+            />
+          </div>
         </div>
         <h1 className={styles["navbar__title"]}>ROAL CASES</h1>
         <div className={styles["navbar__cart-wrapper"]}>
@@ -29,15 +42,26 @@ export const Navbar = () => {
             icon={faShoppingCart}
             className={styles["navbar__cart"]}
           />
+          <h3>
+            Mi Carrito
+          </h3>
         </div>
       </nav>
-        <menu className={`${styles['menu']} ${extraMenu ? styles['menu__active'] : styles['menu__disabled'] } `}>
-          <ul className={styles['menu__items']}>
-            <NavLink />
-          </ul>
-        </menu>
+      <menu
+        className={`${styles["menu"]} ${
+          extraMenu ? styles["menu__active"] : styles["menu__disabled"]
+        } `}
+      >
+        <ul className={styles["menu__items"]}>
+          <NavLink />
+        </ul>
+      </menu>
 
-      <div className={`${styles["offers"]} ${extraMenu ? styles['offers__menu-active'] : ''} `}>
+      <div
+        className={`${styles["offers"]} ${
+          extraMenu ? styles["offers__menu-active"] : ""
+        } `}
+      >
         <span>Oferta En La Compra De Dos Fundas</span>
       </div>
     </header>
