59444de9d7179fcbe703d1840a296f3f033b5c99
Rodrigo Alanis Lemus <rodrigoalanislemus@Rodrigos-MacBook-Air.local>
2022-08-19 23:57:27 -0500

    Pages Refactor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,43 +1,8 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Home } from "./../components/pages/home";
 import type { NextPage } from "next";
-import { Button } from "../components/atoms/shared/Button";
-import { Navbar } from "../components/molecules/shared/Navbar";
-import { Categories } from "../components/organisms/home/Categories";
-import { Hero } from "../components/organisms/home/Hero";
-import { References } from "../components/organisms/home/References";
-import styles from "../styles/pages/Home.module.scss";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
-import { Footer } from "../components/organisms/shared/Footer";
 
-const Home: NextPage = () => {
-  return (
-    <div className={styles["home"]}>
-      <Navbar />
-      <Hero />
-      <div className={styles["home__divider"]}>
-        <h2>Nuestros Productos</h2>
-
-        <Button className={styles["home__btn"]} text="Comprar Ahora" />
-      </div>
-      <Categories />
-      <References />
-      <div className={styles["home__warranty"]}>
-        <div className={styles["home__warranty-icon"]}>
-          <FontAwesomeIcon
-            icon={faCheck}
-            className={styles["home__warranty-check"]}
-          />
-        </div>
-        <h3>Te Garantisamos Calidad</h3>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo a at
-          optio assumenda consequuntur quas, quo ipsam ducimus rem sequi ex et
-          dignissimos neque nemo, ut quaerat dicta recusandae odio.
-        </p>
-      </div>
-      <Footer />
-    </div>
-  );
+const HomePage: NextPage = () => {
+  return <Home />;
 };
 
-export default Home;
+export default HomePage;
